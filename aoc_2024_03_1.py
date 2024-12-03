95d495e8fa816ce5875e9784e2008e4d116189ff
Sven Trampel <sven.trampel@icloud.com>
2024-12-03 23:11:48 +0100

    day 3

---
+++ aoc_2024_03_1.py
@@ -0,0 +1,13 @@
+"""Advent Of Code 2024 - day 1 puzzle 1
+   corrupted code, summ all products mul(v1, v1) """
+#sample 161 (2*4 + 5*5 + 11*8 + 8*5)
+import fileinput, re
+
+res=0
+with fileinput.input(files="input/input_03_sample.txt", encoding="utf-8") as f:
+    for line in f:
+        #muls = re_mul.match(line);
+        for cmd in re.findall(r"mul\(\d+\,\d+\)", line):
+            cmd_mul = re.split(r'[,)(]', cmd)
+            if 'mul' == cmd_mul[0]: res += int(cmd_mul[1])*int(cmd_mul[2])
+print(res)
---
+++ aoc_2024_03_2.py
@@ -0,0 +1,18 @@
+"""Advent Of Code 2024 - day 1 puzzle 1
+   corrupted code, summ all products mul(v1, v1)
+   do() enables, don't() disables next mul, default enabled """
+#sample 48 (2*4 + 8*5)
+import fileinput, re
+
+res=0
+do=True
+with fileinput.input(files="input/input_03.txt", encoding="utf-8") as f:
+    for line in f:
+        #muls = re_mul.match(line);
+        for op in re.findall(r"mul\(\d+\,\d+\)|do\(\)|don't\(\)", line):
+            cmd = re.split(r'[,\)\(]', op)
+            if 'mul' == cmd[0]:
+                if do: res += int(cmd[1])*int(cmd[2])
+            elif 'do' == cmd[0]: do = True
+            elif "don't" == cmd[0]: do = False
+print(res)
